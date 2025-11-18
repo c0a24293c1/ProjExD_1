f18b920e3e951274900f875d2b201e25bc3ad090
watanabe <c0a24293c1@edu.teu.ac.jp>
2025-11-18 14:00:51 +0900

    練習3と4:こうかとんの描画

--- pygame_image.py
+++ pygame_image.py
@@ -11,11 +11,14 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
+    kk_img = pg.image.load("fig/3.png") #練習3
+    kk_img = pg.transform.flip(kk_img, True, False) #練習3
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
