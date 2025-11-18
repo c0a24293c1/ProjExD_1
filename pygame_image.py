5a435ea6d1a639372a4305893c83c23534cbd0cf
watanabe <c0a24293c1@edu.teu.ac.jp>
2025-11-18 14:18:45 +0900

    練習7-9:無限スクロール

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,21 @@ def main():
     tmr = 0
     kk_img = pg.image.load("fig/3.png") #練習3
     kk_img = pg.transform.flip(kk_img, True, False) #練習3
+    bg_img_flip = pg.transform.flip(bg_img, True, False) #練習8
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr, 0]) #練習5
+        screen.blit(bg_img_flip, [-tmr + 1600, 0]) #練習7,練習8
+        screen.blit(bg_img, [-tmr + 3200, 0]) #練習9
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
-        tmr += 1        
-        clock.tick(200)
+        tmr += 1 
+
+        if tmr == 3200: #練習9
+            tmr = 0       
+        clock.tick(200) #練習6
 
 
 if __name__ == "__main__":
